5af76548dda12a720d7a584df59e95af8552ba8b
cooperlappenbusch <coopertrooper7@gmail.com>
2021-05-28 13:01:57 -0700

    Started register code, added utils folder

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,18 +1,65 @@
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
+import { validateRegisterInput, validateLoginInput } from '../../util/validators';
+const saltRounds = 10;
+
+const {
+    UserInputError, // Throw an error if empty fields.
+    AuthenticationError
+} = require('apollo-server');
+
+const getToken = ({ id, username, email }) =>
+  jwt.sign(
+    {
+      id,
+      username,
+      email
+    },
+    SECRET,
+    { expiresIn: '1d' }
+  );
+
 
 module.exports = {
     Mutation: {
         async register(_, {registerInput : {username, email, password, confirmPassword}}) {
+            
+            // Generating hash for password 
+            /*var hash = bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(password, salt, function(err, hash) {
+                    return hash;
+                });
+            });*/
+            const { errors, valid } = validateRegisterInput(username, email, password, confirmPassword);
+            if (!valid) {
+                throw new UserInputError('Error', { errors });
+            }
+
+            const user = await User.findOne({ username });
+            if(user) {
+                throw new ValidationError('Username taken.');
+            }
+
+            password = await bcrypt.hash(password, 10);
+
             const newUser = new User({
                 email,
                 username,
                 password,
                 createdAt: ""
             });
-
             const res = await newUser.save();
-
-            return newUser;
+            const token = getToken(newUser);
+            
+            
+            return {
+                id: res.id,
+                ...res._doc.
+                token
+            };
+            
         }
     },
     Query: {
---
+++ privateVariables.js
@@ -0,0 +1,5 @@
+
+
+module.exports = {
+    privateKey: ":[~L9;=Q?N*jb/0f5l?a'S96m<8?mB"
+}
